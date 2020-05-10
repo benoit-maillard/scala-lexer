6324de134959ed0d5093bf900962695a953970c0
Benoit <benoit.maillard@epfl.ch>
2020-05-10 17:08:05 +0200

    Add documentation

--- src/main/scala/sl/Expressions.scala
+++ src/main/scala/sl/Expressions.scala
@@ -1,14 +1,40 @@
 package sl
 
 object Expressions {
+  /**
+    * Two arbitrary values
+    *
+    * @param left left value
+    * @param right right value
+    */
   case class ~[A, B](left: A, right: B)
 
   type Transform[A] = Seq[String] => A
   type BuildExpr = () => Seq[String]
 
+  /**
+    * Represents the remaining input.
+    *
+    * @param fromStart position from the start of the original input
+    * @param chars sequence of remaining characters
+    */
   case class InputState(val fromStart: Position, val chars: CharSequence)
 
+  /**
+    * A regular expression that supports groups and transformations.
+    *
+    * @param transform function that takes a sequence of strings (matched groups) as input and produces a value
+    * @param build function that defines how the scala standard library regex should be built
+    * @param groupCount total number of groups in the expression
+    */
   class Expr[A](val transform: Transform[A], val build: BuildExpr, val groupCount: Int) {
+    /**
+      * Creates a new expression that matches if the second expression matches directly after the first one.
+      * Groups of each expression are preserved.
+      *
+      * @param right second expression
+      * @return resulting expression
+      */
     def ~[B](right: Expr[B]): Expr[A ~ B] = Expr(
       results => Expressions.~(
         this.transform(results.take(groupCount)),
@@ -18,25 +44,76 @@ object Expressions {
       groupCount + right.groupCount
     )
 
+    /**
+      * Transforms the resulting match value of the expression.
+      * 
+      * For example, an expression can be matched to produce a numeric value instead of a string
+      * {{{
+      * unit("""\d""").map(_.toInt)
+      * }}}
+      *
+      * @param tr transformation function
+      * @return resulting expression
+      */
     def map[B](tr: A => B) = Expr(transform andThen tr, build, groupCount)
   }
 
+  /**
+    * Companion object for Expr
+    */
   object Expr {
+    /**
+      * Creates an expression
+      *
+      * @param transform function that takes a sequence of strings (matched groups) as input and produces a value
+      * @param build function that defines how the scala standard library regex should be built
+      * @param groupCount total number of groups in the expression
+      * @return resulting expression
+      */
     def apply[A](transform: Transform[A], build: BuildExpr, groupCount: Int) =
       new Expr(transform, build, groupCount)
 
-    def unit(re: String): Expr[String] = Expr(results => results.head, () => Seq(Part(re).content), 1)
+      /**
+        * Creates a basic regular expression from a scala standard library regex string
+        *
+        * @param re regex string
+        * @return resulting expression
+        */
+    def unit(re: String): Expr[String] = Expr(results => results.head, () => Seq(re), 1)
   }
 
-  case class Part(val content: String)
-
+  /**
+    * A value and its position
+    *
+    * @param value value
+    * @param pos position of the value
+    */
   case class Positioned[A](val value: A, val pos: Position)
 
+  /**
+    * Position in a multi-line string
+    *
+    * @param index index from the start
+    * @param line line in the string
+    * @param column index from the first character of the line
+    */
   case class Position(val index: Int, val line: Int, val column: Int) {
+    /**
+      * Advances the position to after the given character
+      *
+      * @param char character to add to the position
+      * @return updated position
+      */
     def +(char: Char): Position =
       if (char == '\n') Position(index + 1, line + 1, 0)
       else Position(index + 1, line, column + 1)
 
+    /**
+      * Advances the position to after the given string
+      *
+      * @param seq string to att to the position
+      * @return updated position
+      */
     def +(seq: String): Position = seq.foldLeft(this)((acc, c) => acc + c)
   }
 }
--- src/main/scala/sl/Lexer.scala
+++ src/main/scala/sl/Lexer.scala
@@ -8,23 +8,45 @@ trait Lexers {
   type Value
 
   /**
-    * 
+    * Lexer state at a given time. A state is composed of a set of rules and of a value.
+    * The value can be any value that has some incidence on the tokens to be produced by the lexers.
+    * For example, the value could be the current indentation level in an indentation-based grammar.
     *
-    * @param rules
-    * @param content
+    * @param rules rules that are currently relevant
+    * @param value state content
     */
-  case class LexerState(rules: RuleSet, content: Value)
+  case class LexerState(rules: RuleSet, value: Value)
+
+  /**
+    * Result of matching a given rule with input prefix.
+    *
+    * @param tokens tokens produced by matching
+    * @param state state produced by matching
+    * @param inputState progression in input string
+    */
   case class RuleMatch(tokens: List[Positioned[Token]], state: LexerState, inputState: InputState)
 
+  /**
+    * Lexer that can produce tokens given an input string.
+    *
+    * @param initialState set of rules and value that can be matched at the start of input string
+    */
   case class Lexer(initialState: LexerState) {
+    /**
+      * Produces tokens using the successive rules and the given input.
+      *
+      * @param input string containing tokens
+      * @return produced tokens if matching rules were found for the entire input, None otherwise
+      */
     def tokenize(input: String): Option[List[Positioned[Token]]] = {
       val start = InputState(Position(0, 1, 0), new ArrayCharSequence(input.toArray))
       advance(start, initialState, Nil).map(s => s.reverse)
     }
     
+    // uses successive rules to make progress with input
     @tailrec
     private def advance(input: InputState, state: LexerState, acc: List[Positioned[Token]]): Option[List[Positioned[Token]]] =
-      state.rules.firstMatch(input, state.content) match {
+      state.rules.firstMatch(input, state.value) match {
         case None => None
         case Some(RuleMatch(tokens, nextState, remainingInput)) =>
           if (remainingInput.chars.length == 0) Some(tokens ++ acc)
@@ -32,8 +54,22 @@ trait Lexers {
       }
   }
 
-  // should contain the type of the state (exemple: level of parenthesis or indentation stack)
+  /**
+    * Ordered set of rules for matching against input. The order in which rules are given is the
+    * order of priority in which they are matched. If rule A has higher priority than B, only
+    * rule A is taken into account if it is a match, even if B is a match as well.
+    *
+    * @param rules
+    */
   case class RuleSet(val rules: Rule[_]*) {
+    /**
+      * Tries to match the rules of the set against the input in priority order recursively.
+      *
+      * @param input input against which rules must be matched
+      * @param value state value before attempting to match
+      * @param remainingRules rules against which matching is yet to be attempted
+      * @return a RuleMatch instance if any of the rules is matching the input prefix, None otherwise
+      */
     def firstMatch(input: InputState, value: Value, remainingRules: Seq[Rule[_]] = rules): Option[RuleMatch] = remainingRules match {
       case Seq() => None
       case r +: rs => r.tryTransition(LexerState(this, value), input) match {
@@ -42,10 +78,28 @@ trait Lexers {
       }
     }
 
+    /**
+      * Creates a new state based on the current set and a given value.
+      *
+      * @param value value for the state
+      * @return State based on the current state and the value
+      */
     def apply(value: Value): LexerState = LexerState(this, value)
   }
 
+  /**
+    * Rule associating a regular expression with a transition function
+    *
+    * @param expr regular expression that is matched against the input
+    */
   abstract class Rule[ExprRes](expr: CompiledExpr[ExprRes]) {
+    /**
+      * Tries to match the rule expression against the input.
+      *
+      * @param state state before the matching is attempted
+      * @param input input against which matching is attempted
+      * @return a RuleMatch instance if the regular expression is matching the input prefix, None otherwise
+      */
     def tryTransition(state: LexerState, input: InputState): Option[RuleMatch] =
       expr.matchWith(input) match {
         case None => None
@@ -55,31 +109,61 @@ trait Lexers {
           Some(RuleMatch(producedTokens.reverse, newState, InputState(endPos, remainingChars)))
         }
       }
-
-    def transitionResult(state: LexerState, matchVal: ExprRes, input: Expressions.InputState): (LexerState, List[Positioned[Token]])
+    
+    // gets the result of the transition associated with the rule
+    protected def transitionResult(state: LexerState, matchVal: ExprRes, input: InputState): (LexerState, List[Positioned[Token]])
   }
 
+  
   type StandardTransition[ExprRes] = (Value, ExprRes, Position) => (LexerState, List[Positioned[Token]])
 
+  /**
+    * Rule with a transition from a state to any other state
+    *
+    * @param expr regular expression that is matched against the input
+    * @param transition transition to any other state
+    */
   case class StandardRule[ExprRes](expr: CompiledExpr[ExprRes], transition: StandardTransition[ExprRes]) extends Rule[ExprRes](expr) {
-    override def transitionResult(state: LexerState, matchVal: ExprRes, input: Expressions.InputState) =
-      transition(state.content, matchVal, input.fromStart)
+    override def transitionResult(state: LexerState, matchVal: ExprRes, input: InputState) =
+      transition(state.value, matchVal, input.fromStart)
   }
 
   type ReflectiveTransition[ExprRes] = (Value, ExprRes, Position) => (Value, List[Positioned[Token]])
 
+  /**
+    * Rule with a transition from a state to a state with the same rules as previously
+    *
+    * @param expr regular expression that is matched against the input
+    * @param transition transition to a state with same rules as previously
+    */
   case class ReflectiveRule[ExprRes](expr: CompiledExpr[ExprRes], transition: ReflectiveTransition[ExprRes]) extends Rule[ExprRes](expr) {
-    override def transitionResult(state: LexerState, matchVal: ExprRes, input: Expressions.InputState) = {
-      val (newValue, producedTokens) = transition(state.content, matchVal, input.fromStart)
+    override def transitionResult(state: LexerState, matchVal: ExprRes, input: InputState) = {
+      val (newValue, producedTokens) = transition(state.value, matchVal, input.fromStart)
       (LexerState(state.rules, newValue), producedTokens)
     }
   }
 
+  /**
+    * Top-level, compiled regular expression
+    *
+    * @param expr underlying expression
+    */
   class CompiledExpr[A](expr: Expr[A]) {
     val re = expr.build().map(s => "(" + s + ")").reduce(_ + _).r
 
-    def get() = re
+    /**
+      * The regular expression in its scala standard library form
+      *
+      * @return regex
+      */
+    def get: Regex = re
 
+    /**
+      * Tries to match the expression against the input
+      *
+      * @param input input against which the regex is matched
+      * @return the match result and the position after the entire match in input
+      */
     def matchWith(input: InputState): Option[(A, Position)] = re.findPrefixMatchOf(input.chars) match {
       case None => None
       case Some(m) => {
@@ -88,9 +172,21 @@ trait Lexers {
         Some(expr.transform(m.subgroups), afterPos)
       }
     }
-
+    
+    /**
+      * Associates the expression with a transition to any state to create a rule
+      *
+      * @param transform transformation function
+      * @return resulting rule
+      */
     def |~>[Token, Value](transform: StandardTransition[A]) = StandardRule(this, transform)
 
+    /**
+      * Associated the expression with a transition to a state with the same set of rules to create a rule
+      *
+      * @param transform transformation function
+      * @return resulting rule
+      */
     def |>[Token, Value](transform: ReflectiveTransition[A]) = ReflectiveRule(this, transform)
   }
 }
